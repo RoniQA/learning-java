db3d6e4c5a63ffd7179bbce4a190b1ad07437a3d
RoninQA <ronierisoncosta@gmail.com>
2022-03-23 18:10:47 -0300

    Finalizacao de logica com Java

---
+++ src/aula_04_funcoes/Funcao02.java
@@ -0,0 +1,10 @@
+package aula_04_funcoes;
+
+public class Funcao02 {
+    public static void main(String[] args){
+
+        System.out.println("Vai começar a contagem");
+        System.out.println(Operacoes.contador(1,5));
+
+    }
+}
